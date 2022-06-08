c01a1638a68cb58cb6714faf3830b6f769b0eb26
danielsychoo <sungyeop.choo@gmail.com>
2022-06-08 14:36:22 +0900

    [Settings] Delete storyBoard & make RootViewController with Navigation

---
+++ GithubRepository/RepositoryListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryListViewController.swift
+//  GithubRepository
+//
+//  Created by sungyeopTW on 2022/06/08.
+//
+
+import UIKit
+
+class RepositoryListViewController: UITableViewController {
+    
+}
